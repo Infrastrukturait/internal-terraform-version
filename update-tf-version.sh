42681c56f1e11405b099b7769571edc1c9d85448
Rafał Masiarek <rafal@masiarek.pl>
2022-12-19 18:23:06 +0100

    add commit

--- update-tf-version.sh
+++ update-tf-version.sh
@@ -7,3 +7,6 @@ TG_VERSION=$(cat ./terragrunt-version)
 echo $TF_VERSION > terraform-version
 
 sed -e s/%TF_VERSION%/$TF_VERSION/g -e s/%TG_VERSION%/$TG_VERSION/g ./README.md.template > ./README.md
+
+git add -A
+git commit -m "update tf to `$TF_VERSION`"
--- update-tg-version.sh
+++ update-tg-version.sh
@@ -7,3 +7,6 @@ TG_VERSION=$1
 echo $TG_VERSION > terragrunt-version
 
 sed -e s/%TF_VERSION%/$TF_VERSION/g -e s/%TG_VERSION%/$TG_VERSION/g ./README.md.template > ./README.md
+
+git add -A
+git commit -m "update tg to `$TG_VERSION`"
