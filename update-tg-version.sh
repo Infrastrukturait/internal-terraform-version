d23106221c2c8e95eb32518596c6816a952a9342
Rafał Masiarek <rafal@masiarek.pl>
2022-12-19 18:21:20 +0100

    chore: add support to terragrunt

--- update-tg-version.sh
+++ update-tg-version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-TF_VERSION=$1
-TG_VERSION=$(cat ./terragrunt-version)
+TF_VERSION=%(cat ./terraform-version)
+TG_VERSION=$1
 
-echo $TF_VERSION > terraform-version
+echo $TG_VERSION > terragrunt-version
 
 sed -e s/%TF_VERSION%/$TF_VERSION/g -e s/%TG_VERSION%/$TG_VERSION/g ./README.md.template > ./README.md
